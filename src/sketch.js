2812300daa5013ef5894b06a9297f90919ca43da
Alex Knight <alex.knight@leavittpartners.com>
2018-06-29 15:25:25 -0600

    Refactor to allow for representing any affiliation
    Also converted to using the http methods built into P5, added an auth token cookie and cleaned up some bugs.

--- src/sketch.js
+++ src/sketch.js
@@ -3,113 +3,138 @@ var edges = [];
 var data = '[{"name":"Garfield County","id":"257096","Percent of Total Hospital Referral Volume by Group":"32.3%"},{"name":"Ihc Health Services, Inc.","id":"137212","Percent of Total Hospital Referral Volume by Group":"23.8%"},{"name":"Wayne Community Health Centers Inc","id":"55942","Percent of Total Hospital Referral Volume by Group":"20.6%"},{"name":"Cedar City Radiology Llc","id":"16543","Percent of Total Hospital Referral Volume by Group":"6.4%"},{"name":"Cedar Anesthesia Group Llp","id":"7295","Percent of Total Hospital Referral Volume by Group":"5.5%"},{"name":"Foot And Ankle Institute, Inc.","id":"204286","Percent of Total Hospital Referral Volume by Group":"2.4%"},{"name":"Dr. Randy G Delcore, M.D. P.C.","id":"221194","Percent of Total Hospital Referral Volume by Group":"1.9%"},{"name":"Beaver Medical, Llc","id":"165594","Percent of Total Hospital Referral Volume by Group":"1.4%"},{"name":"Central Utah Clinic Pc","id":"285132","Percent of Total Hospital Referral Volume by Group":"1.3%"},{"name":"Kane County Human Resource Special Service District","id":"226821","Percent of Total Hospital Referral Volume by Group":"0.8%"},{"name":"Robert D Pearson Pc","id":"254412","Percent of Total Hospital Referral Volume by Group":"0.5%"},{"name":"St George Urology Llc","id":"180238","Percent of Total Hospital Referral Volume by Group":"0.3%"},{"name":"Huntsman West Spine Llc","id":"8768","Percent of Total Hospital Referral Volume by Group":"0.3%"},{"name":"Dialysis Program Physicians University Of Utah","id":"210779","Percent of Total Hospital Referral Volume by Group":"0.2%"},{"name":"University Of Utah Adult Services","id":"22859","Percent of Total Hospital Referral Volume by Group":"0.2%"},{"name":"Enterprise Valley Medical Clinic, Inc.","id":"267522","Percent of Total Hospital Referral Volume by Group":"0.1%"},{"name":"Karen Murray Radley M.D.Pc","id":"294084","Percent of Total Hospital Referral Volume by Group":"0.1%"},{"name":"St. George Radiology Inc.","id":"278298","Percent of Total Hospital Referral Volume by Group":"0.1%"},{"name":"Tooele Valley Imaging Llc","id":"169445","Percent of Total Hospital Referral Volume by Group":"0.1%"},{"name":"San Juan County Hospital","id":"212970","Percent of Total Hospital Referral Volume by Group":"0.1%"},{"name":"Beaver Valley Hospital","id":"174531","Percent of Total Hospital Referral Volume by Group":"0%"},{"name":"Advanced Pulmonary, Sleep Disorder And Internal Medicine,Llc","id":"186979","Percent of Total Hospital Referral Volume by Group":"0%"},{"name":"Portercare Adventist Health System","id":"20698","Percent of Total Hospital Referral Volume by Group":"0%"},{"name":"Catholic Health Initiatives Colorado","id":"297077","Percent of Total Hospital Referral Volume by Group":"0%"}]';
 var hospData = '[{"name":"Garfield Memorial Hospital"}]';
 var authToken;
+var datasetFields = {
+  'hospital' : {
+    'id' : 'torch_hospital_id',
+    'primaryname' : 'name',
+    'affiliationname' : 'name'
+  },
+  'physiciangroup' : {
+    'id' : 'torch_provider_group_id',
+    'primaryname' : 'organizationlegalname',
+    'affiliationname' : 'name'
+  }
+}
 
 function preload() {
   //Use dummy data when testing to not constantly ping the server
   //populateGraph(JSON.parse(data), JSON.parse(hospData));
-  sendHttpRequest('GET',
+  // let credentials = loadJSON('../credentials.json');
+  // authenticate(credentials.email, credentials.password);
+  httpDo(
     '../credentials.json',
-    [
-      {header:'Content-Type', value:'application/json'}
-    ],
-    null,
+    {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    },
     request => {
-      let response = JSON.parse(request.responseText);
-      authenticate(response.email, response.password);
+      authenticate(request.email, request.password);
     }
-  );
+  )
 }
 
 function authenticate(email, password) {
-  sendHttpRequest('POST', 
-    'https://api.torchinsight.com/authenticate',
-    [
-      {header:'Content-Type', value:'application/json'}
-    ],
-    '{"email":"' + email + '","password":"' + password + '"}',
-    request => {
-      authToken = request.responseText;
-      getAffiliationData(document.querySelector('#hospitalid').value);
-    }
-  );
-}
-
-function getAffiliationData(id) {
-  sendHttpRequest('GET',
-    'https://api.torchinsight.com/data/hospital/' + id + '/affiliations/physiciangroup',
-    [
-      {header:'Content-Type', value:'application/json'},
-      {header:'Authorization', value:'Bearer ' + authToken}
-    ],
-    null,
-    request => {
-      getHospitalData(id, JSON.parse(request.responseText));
-    }
-  );
-}
-
-function getHospitalData(id, affiliationData) {
-  sendHttpRequest('POST', 
-    'https://api.torchinsight.com/data/hospital/',
-    [
-      {header:'Content-Type', value:'application/json'},
-      {header:'Authorization', value:'Bearer ' + authToken}
-    ],
-    '{"fields":["name"],"filters":[{"field":"torch_hospital_id","test":"=","value":' + id + '}]}',
-    request => {
-      populateGraph(affiliationData, JSON.parse(request.responseText));
-    }
-  );
-}
-
-function sendHttpRequest(type, url, headers, payload, callback) {
-  let httpRequest = new XMLHttpRequest();
-  httpRequest.open(type, url, true);
-
-  headers.forEach(e => {
-    httpRequest.setRequestHeader(e.header, e.value);
-  });
-
-  httpRequest.onreadystatechange = () => {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        callback(httpRequest);
-      } else {
-        alert('There was a problem with the request.');
+  authToken = getCookie('Token');
+  if (authToken == "") {
+    httpDo('https://api.torchinsight.com/authenticate',
+      {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: '{"email":"' + email + '","password":"' + password + '"}'
+      },
+      request => {
+        authToken = request;
+        setCookie("Token", authToken, 1);
+        getData(document.querySelector('#entityid').value, 'hospital', 'physiciangroup');
       }
-    }
+    );
+  } else {
+    getData(document.querySelector('#entityid').value, 'hospital', 'physiciangroup');
   }
-
-  payload == null ? httpRequest.send() : httpRequest.send(payload);
 }
 
-function populateGraph(entityArray, hospitalData) {
-  nodes.push(new Entity(window.innerWidth / 2 - 50, window.innerHeight / 2 - 50, hospitalData[0].name, 100 ));
+function getData(id, fromDataSource, toDataSource) {
+  let affiliationData;
+  let primaryEntityData;
+  
+  Promise.all([
+    getAffiliationData(id, fromDataSource, toDataSource, data => { affiliationData = data }),
+    getPrimaryEntityData(id, fromDataSource, data => { primaryEntityData = data })
+  ]).then(() => {
+    populateGraph(affiliationData, primaryEntityData, toDataSource, fromDataSource);
+    redraw();
+  });
+}
 
-  let filtered = entityArray.filter(e => parseInt(e['Percent of Total Hospital Referral Volume by Group']) >= 1);
-  let nodeAngle = radians(360 / filtered.length);
+function getAffiliationData(id, fromDataSource, toDataSource, callback) {
+  return new Promise((resolve, reject) => {
+    httpDo(
+      'https://api.torchinsight.com/data/' + fromDataSource + '/' + id + '/affiliations/' + toDataSource,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + authToken
+        }
+      },
+      request => {
+        resolve(callback(JSON.parse(request)));
+      }
+    );
+  });
+}
+
+function getPrimaryEntityData(id, entityDataSource, callback) {
+  return new Promise((resolve, reject) => {
+    httpDo(
+      'https://api.torchinsight.com/data/' + entityDataSource + '/',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + authToken
+        },
+        body: '{"fields":["' + datasetFields[entityDataSource].primaryname + '"],"filters":[{"field":"' + datasetFields[entityDataSource].id + '","test":"=","value":' + id + '}]}'
+      },
+      request => {
+        resolve(callback(JSON.parse(request)));
+      }
+    );
+  });
+}
+
+function populateGraph(affiliatedEntityArray, primaryEntityData, affiliatedEntityType, primaryEntityType) {
+  const centerX = window.innerWidth / 2 - 50;
+  const centerY = window.innerHeight / 2 - 50;
+  nodes.push(new Entity(centerX, centerY, 100, primaryEntityData[0][datasetFields[primaryEntityType].primaryname], primaryEntityType));
+
+  const filtered = affiliatedEntityArray.filter(e => parseInt(e[Object.keys(e)[2]]) >= 1);
+  const nodeAngle = (2 * PI) / filtered.length;
 
   filtered.forEach((e, i) => {
-    let size = parseInt(e['Percent of Total Hospital Referral Volume by Group']);
-    let nodeX = (window.innerWidth / 2 - (size + 50) / 2) + (300 * cos((nodeAngle * i) - PI/2));
-    let nodeY = (window.innerHeight / 2 - (size + 50) / 2) + (300 * sin((nodeAngle * i) - PI/2));
-    nodes.push(new WeightedEntity(nodeX, nodeY, e.name, size, size + 50 ));
+    const size = parseInt(e[Object.keys(e)[2]]);
+    const radius = size + 50;
+    const nodeX = centerX + (300 * cos((nodeAngle * i) - PI/2));
+    const nodeY = centerY + (300 * sin((nodeAngle * i) - PI/2));
+    console.log(e);
+    nodes.push(new WeightedEntity(nodeX, nodeY, radius, e[datasetFields[affiliatedEntityType].affiliationname], affiliatedEntityType, size ));
   });
 
   nodes.slice(1).forEach(e => {
     edges.push(new Edge(nodes[0], e));
-  })
+  });
 }
 
 function updateDataForID() {
   nodes = [];
   edges = [];
 
-  getAffiliationData(document.querySelector('#hospitalid').value);
+  getData(document.querySelector('#entityid').value, 'physiciangroup', 'hospital');
 }
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  noLoop();
 }
 
 function draw() {
@@ -135,7 +160,7 @@ class GraphNode {
 }
 
 class Entity extends GraphNode {
-  constructor(x, y, entityName, radius) {
+  constructor(x, y, radius, entityName, entityType) {
     super(x, y, radius);
     this.entityName = entityName;
   }
@@ -152,8 +177,8 @@ class Entity extends GraphNode {
 }
 
 class WeightedEntity extends Entity {
-  constructor(x, y, entityName, entityWeight, radius) {
-    super(x, y, entityName, radius);
+  constructor(x, y, radius, entityName, entityType, entityWeight) {
+    super(x, y, radius, entityName);
     this.entityWeight = entityWeight;
   }
 
@@ -179,4 +204,27 @@ class Edge {
     let theta = atan((r.y - l.y)/(r.x - l.x))
     line(l.x + (l.radius * cos(theta)), l.y + (l.radius * sin(theta)), r.x - (r.radius * cos(theta)), r.y - (r.radius * sin(theta)));
   }
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
 }
